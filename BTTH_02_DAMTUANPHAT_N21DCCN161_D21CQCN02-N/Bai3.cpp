5bb4b9e562ab6e3a6e884a38f1f35ba0d38c8aa3
DamTuanPhat <damtuanphat@gmail.com>
2022-09-15 08:14:11 +0700

    Complete Practice 2

--- BTTH_02_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai3.cpp
+++ BTTH_02_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai3.cpp
@@ -4,8 +4,25 @@
 
 using namespace std;
 
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void sap_xep(int *a, int length) {
+	for(int i = 0; i < length-1; i++) {
+		for(int j = i+1; j < length; j++) {
+			if(*(a+i) < *(a+j)) {
+				// cout << *(a+i) << " " << *(a+j) << endl;
+				swap((a+i), (a+j));
+			}
+		}
+	}
+}
+
 int main() {
-    const int n = 500;
+    const int n = 1000;
     int a[n];
     for(int i = 0; i < n; i++) {
         a[i] = rand() - (RAND_MAX/2);
@@ -16,18 +33,27 @@ int main() {
     int size = 0;
     for(int i = 0; i < n; i++) {
         if (a[i] > 0) {
-            b[size] = a[i];
-            size++;
+            b[size++] = a[i];
         }
+
     }
     
-    cout << endl << endl;
-    cout << size << endl << endl;
+    cout << endl << endl << endl << endl;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < size; i++) {
         cout << b[i] << " ";
     }
 
+    cout << endl << endl << endl << endl;
+
+    cout << "Nhap x tu ban phim: ";
+    cin >> b[size++];
+    cout << endl ;
+
+    cout << "Mang sau khi them phan tu: " << endl;
+    for(int i = 0; i < size; i++) {
+        cout << b[i] << " ";
+    }
 
     return 0;
 }
--- BTTH_02_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai4.cpp
+++ BTTH_02_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai4.cpp
@@ -1,9 +1,68 @@
 #include<iostream>
+#include <math.h> 
 
 using namespace std;
 
-int main() {
-    
+int binarySearch(int T, int a[], int length) {
+    int result;
+    int left = 0, right = length - 1;
 
+    while (left < right)
+    {
+        int m = floor((left + right) / 2);
+        if (a[m] < T) {
+            left = m + 1;
+        }
+        else if (a[m] > T) {
+            right = m - 1;
+        }
+        else if (a[m] == T) {
+            return m;
+        }
+    }
+    
+    return -1;
+}
+
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void sap_xep(int *a, int length) {
+	for(int i = 0; i < length-1; i++) {
+		for(int j = i+1; j < length; j++) {
+			if(*(a+i) < *(a+j)) {
+				// cout << *(a+i) << " " << *(a+j) << endl;
+				swap((a+i), (a+j));
+			}
+		}
+	}
+}
+
+int main() {
+	int n;
+	cout << "Nhap vao so nguyen duong n: ";
+	cin >> n;
+	
+	int arr[n];
+	for(int i = 0; i < n; i++) {
+		cout << "Nhap gia tri phan tu thu " << i << " = ";
+		cin >> arr[i];
+	}
+
+    int x;
+	cout << endl << "Nhap gia tri tim kiem: ";
+    cin >> x;
+
+    if(binarySearch(x, arr, n) != -1) {
+        cout << "Co xuat hien gia tri trong mang!" << endl;
+    }
+    else {
+        cout << "Khong co xuat hien gia tri trong mang!" << endl;
+
+    }
+	
     return 0;
 }
