1b2dcd1e559315714e1700c2e04f54aa341be17a
DamTuanPhat <damtuanphat@gmail.com>
2022-09-21 23:16:35 +0700

    Complete chapter 04

---
+++ BT_04_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai14.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+float tongNghichDao(int arr[], int n) {
+    float sum = 0;
+    for(int i = 1; i < n; i++) {
+        sum += 1/(float) i;
+    }
+
+    return sum;
+}
+
+void daoChieu(int arr[], int n) {
+    for(int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+}
+
+int fibonacci(int n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Nhap vao so phan tu fibonacci: ";
+    cin >> n;
+    int arr[n];
+    
+    for(int i = 0; i < n; i++) {
+        arr[i] = fibonacci(i);
+    }
+    
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << "Tong nghich dao cua " << n - 1 << " phan tu dau tien la: " << tongNghichDao(arr, n) << endl;
+
+    cout << endl << "Sau khi dao chieu: " << endl;
+    daoChieu(arr,n);
+
+    return 0;
+}
