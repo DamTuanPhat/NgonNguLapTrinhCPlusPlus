31b90b1b29f649f663e14906e8744ab912f15869
FungoMan <phuongnamtran1902@gmail.com>
2022-10-07 00:14:21 +0700

    update bai4 chuong 6

--- BT_06_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai4.cpp
+++ BT_06_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai4.cpp
@@ -1,58 +1,60 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <numeric>
+#include <map>
+#include <iomanip>
+#include <set>
 
 using namespace std;
-
-void standard(char s[]) {
-    int size = strlen(s);
-    int totalCount = 0;
-    for(int i = 0; i < size; i++) {
-        if(s[i] == ' ') {
-            totalCount++;
+#define int long long
+#define endl "\n"
+const int mod = 1e9 + 7;
+const int MIN = mod;
+const int MAX = -1;
+void xoa(char a[])
+{
+    vector<int> v;
+    bool ok = false;
+    for (int i = 0; i < strlen(a); i++)
+    {
+        if (!ok && a[i] == ' ')
+        {
+            v.push_back(i);
+            ok = 1;
         }
-        
+        else if (a[i] != ' ')
+        {
+            v.push_back(i);
+            ok = 0;
+        }
+        else if (a[i] == ' ' && ok == 1) continue;
+           
     }
-    int count = 0;
-    for(int i = 0; i < size; i++) {
-        if(s[i] != ' ') {
-            break;
-        }
-        if(++count < totalCount) {
-            for(int j = i; j < size; j++) {
-                s[j] = s[j+1];
-            }
-        }
-        
-    }
-    for(int i = size - 1; i >= 0; i--) {
-        if(s[i] != ' ') {
-            break;
-        }
-        if(++count < totalCount) {
-            size--;
-        }
-    }
-
-    for(int i = 0; i < size; i++) {
-        if(s[i] == ' ') {
-            if(++count < totalCount) {
-                for(int j = i; j < size; j++) {
-                    s[j] = s[j+1];
-                }
-            }
+    if (!ok)
+    {
+        if(a[0] != ' ') cout << a[0];
+        for (int i = 1; i < v.size(); i++)
+        {
+            cout << a[v[i]];
         }
     }
-
-    for(int i = 0; i < size; i++) {
-        cout << s[i];
-    }
+    
+    else {
+        if(a[0] != ' ') cout << a[0];
+         for (int i = 1; i < v.size() - 1; i++)cout << a[v[i]];
+    }  
+            
 }
 
-int main() {
-    char s[100];
-    cout << "Nhap xau: ";
-    cin.getline(s, 100);
-
-    standard(s);
-    return 0;
+signed main()
+{
+    char a[100];
+    cin.getline(a, 100);
+    xoa(a);
 }
