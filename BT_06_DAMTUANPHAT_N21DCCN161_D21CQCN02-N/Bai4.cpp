85eb64b987f1f6147e4eb43f8235e467e4f0f875
DamTuanPhat <damtuanphat@gmail.com>
2022-10-08 10:20:50 +0700

    Update bai 4 chuong 6

--- BT_06_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai4.cpp
+++ BT_06_DAMTUANPHAT_N21DCCN161_D21CQCN02-N/Bai4.cpp
@@ -1,54 +1,74 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 
 using namespace std;
 
-void standard(char s[]) {
+void standard(char s[])
+{
     int size = strlen(s);
     int totalCount = 0;
-    for(int i = 0; i < size; i++) {
-        if(s[i] == ' ') {
+    for (int i = 0; i < size; i++)
+    {
+        if (s[i] == ' ')
+        {
             totalCount++;
         }
-        
     }
+
     int count = 0;
-    for(int i = 0; i < size; i++) {
-        if(s[i] != ' ') {
+
+    for (int i = 0; i < size; i++)
+    {
+        if (s[i] != ' ')
+        {
             break;
         }
-        if(++count < totalCount) {
-            for(int j = i; j < size; j++) {
-                s[j] = s[j+1];
+        if (++count < totalCount)
+        {
+            for (int j = i; j < size; j++)
+            {
+                s[j] = s[j + 1];
             }
         }
-        
     }
-    for(int i = size - 1; i >= 0; i--) {
-        if(s[i] != ' ') {
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (s[i] != ' ')
+        {
             break;
         }
-        if(++count < totalCount) {
+        if (++count < totalCount)
+        {
             size--;
         }
     }
 
-    for(int i = 0; i < size; i++) {
-        if(s[i] == ' ') {
-            if(++count < totalCount) {
-                for(int j = i; j < size; j++) {
-                    s[j] = s[j+1];
+    for (int k = 0; k < totalCount; k++)
+    {
+
+        for (int i = 0; i < size; i++)
+        {
+            if (s[i] == ' ')
+            {
+                if (++count < totalCount)
+                {
+                    for (int j = i; j < size; j++)
+                    {
+                        s[j] = s[j + 1];
+                    }
                 }
             }
         }
     }
 
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << s[i];
     }
 }
 
-int main() {
+int main()
+{
     char s[100];
     cout << "Nhap xau: ";
     cin.getline(s, 100);
